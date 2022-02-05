532ef7f4d65d0a99fb99740b6ec97a36d5c68aa1
Darshan-Kavalakatt <75749929+Darshan-Kavalakatt@users.noreply.github.com>
2022-02-05 13:28:56 +0000

    Added multiple files
    Added serializers
    added urls
    added get/post api views

--- signly/signlyAPI/views.py
+++ signly/signlyAPI/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from rest_framework import status
+
+from .models import Letter
+from .serializers import *
 
 # Create your views here.
+
+@api_view(['GET', 'POST'])
+def students_list(request):
+    if request.method == 'GET':
+        alpha = request.data.get("letter")
+        data = Letter.objects.filter(letter = alpha)
+
+        serializer = LetterSerializer(data, context={'request': request}, many=True)
+
+        return Response(serializer.data)
+
