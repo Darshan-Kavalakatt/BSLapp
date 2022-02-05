3a6a91d6e3986929cea0ba42823c6f4b217fd5ed
Bruce Lay <33815726+brucelay@users.noreply.github.com>
2022-02-05 13:49:14 +0000

    Typo fix

--- signly/signlyAPI/views.py
+++ signly/signlyAPI/views.py
@@ -8,13 +8,14 @@ from .serializers import *
 
 # Create your views here.
 
+
 @api_view(['GET', 'POST'])
-def students_list(request):
+def letters_list(request):
     if request.method == 'GET':
         alpha = request.data.get("letter")
-        data = Letter.objects.filter(letter = alpha)
+        data = Letter.objects.filter(letter=alpha)
 
-        serializer = LetterSerializer(data, context={'request': request}, many=True)
+        serializer = LetterSerializer(
+            data, context={'request': request}, many=True)
 
         return Response(serializer.data)
-
