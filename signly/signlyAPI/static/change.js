e8b6a1e6255b08f426b48e2473cf5a1079de7c97
Darshan-Kavalakatt <75749929+Darshan-Kavalakatt@users.noreply.github.com>
2022-02-05 21:39:04 +0000

    added whale stuffs
    WHALE

--- signly/signlyAPI/static/change.js
+++ signly/signlyAPI/static/change.js
@@ -8,3 +8,31 @@ async function sign(image, letters) {
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+async function checkAPI(word){
+    response = await fetch("http://127.0.0.1:8000/api/videolink?word="+word)
+    if(response.status == 200) {
+        text = await response.text()
+        return text;
+    } else {
+        return false;
+    }
+}
+
+async function wordsplitter(image,video,letters){
+    var words = letters.split(" ");
+    for(let i = 0; i< words.length; i++){
+        if(checkAPI(words[i])==false){
+            for(let y = 0;y<words[i].length;y++){
+                await sleep(500);
+                image.src = "http://127.0.0.1:8000/api/letters/?letters=" + words[i][y]
+            }
+        }else{
+            video.src = checkAPI(words[i]);
+        }
+    }
+    await sleep(1500);
+    
+
+
+}
