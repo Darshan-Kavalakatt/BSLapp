65578c91ad3d12407645cd4252c11798088fdbaf
Bruce Lay <33815726+brucelay@users.noreply.github.com>
2022-02-05 15:26:46 +0000

    Fix letters API

--- signly/signlyAPI/views.py
+++ signly/signlyAPI/views.py
@@ -3,12 +3,12 @@ path = "/image"
 #from templates import *
 
 # Create your views here.
+
+
 def letters_list(request):
     param = request.GET.get('letters')
     print(param)
-    model_image = path+"/"+(param)+".jpg" #get an instance of model which has an ImageField
-    context = {'image' : model_image }
-    html = render(request , 'some_html.html' , context)
-
-
-
+    # get an instance of model which has an ImageField
+    model_image = path + "/" + (param) + ".jpg"
+    context = {'image': model_image}
+    return render(request, 'some_html.html', context)
