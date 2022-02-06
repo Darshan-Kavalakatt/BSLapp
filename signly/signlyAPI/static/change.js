7fa8149b6105ea6fa913463e9076392cb825fab6
unknown <ZJAC219@live.rhul.ac.uk>
2022-02-06 00:46:10 +0000

    Changed the size of the finger spelling pictures and increased the time between each image to 750ms.

--- signly/signlyAPI/static/change.js
+++ signly/signlyAPI/static/change.js
@@ -33,7 +33,7 @@ async function wordsplitter(image,video,letters){
             for(let y = 0;y<words[i].length;y++){
                 image.src = "http://127.0.0.1:8000/api/letters/?letters=" + words[i][y]
                 image.style.display = "block"; 
-                await sleep(500);
+                await sleep(750);
                 image.style.display = "none";
             }
         }else{
